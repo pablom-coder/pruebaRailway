48bc31005f1f99fd8be3add18b790a6e09aa974b
pablom-coder <npmanrique@gmail.com>
2023-01-31 17:44:42 -0300

    Cambios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import Server from "./services/server.js";
 import { initMongoDB } from "./services/database.js";
+import { application } from "express";
 
-const init = async () => {
+/* import express from 'express';
+const app = express(); */
+
+/* const init = async () => {
   await initMongoDB();
   const puerto = process.env.PORT || 8080;
 
@@ -11,6 +15,25 @@ const init = async () => {
   Server.on("error", (error) => {
     console.log("Catch de error en servidor!", error);
   });
-};
-
+}; 
 init();
+*/
+
+const init = async () => {
+  //await initMongoDB();
+  const puerto = process.env.PORT || 8080;
+
+  Server.listen(puerto, () => console.log(`Server up en puerto ${puerto}`));
+
+  Server.on("error", (error) => {
+    console.log("Catch de error en servidor!", error);
+  });
+}; 
+init();
+/* const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+  console.log(`Server en el puerto ${PORT}`)
+})
+ */
+
