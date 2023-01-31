952bb10f4662eb34f8b5e34c31b863e90aa16a4e
pablom-coder <npmanrique@gmail.com>
2023-01-31 18:03:14 -0300

    Cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,8 @@ router.use("/productos", productsRouter);
 
 router.use("/mensajes", messagesRouter);
 
+router.get('/', (req,res)=>{
+    res.send('<h1> Bienvenidos a mi App </h1>')
+});
+
 export default router;
